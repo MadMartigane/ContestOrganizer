5ba4bddadba57ec639f2c84e1045051479f14670
Mad Martigane <martigane.mad@gmail.com>
2023-04-22 17:20:27 +0000

    fix base-url by external hack :-/

--- stencil.config.ts
+++ stencil.config.ts
@@ -6,12 +6,13 @@ export const config: Config = {
   globalStyle: 'src/global/app.css',
   globalScript: 'src/global/app.ts',
   taskQueue: 'async',
+  buildEs5: false,
   outputTargets: [
     {
       type: 'www',
       // comment the following line to disable service workers in production
       serviceWorker: null,
-      baseUrl: 'https://myapp.local/',
+      baseUrl: '/',
     },
   ],
 };
