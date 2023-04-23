e44d3d68a903789ec33eb6408355868c1e78bfb9
Michaël Baudchon <michael.baudchon@ovhcloud.com>
2023-04-23 12:14:22 +0200

    Add go ranking

--- src/modules/TeamRow.type.ts
+++ src/modules/TeamRow.type.ts
@@ -1,3 +1,11 @@
+export interface TeamRowProperties {
+  id: number;
+  name?: string;
+  points: number;
+  concededGoals: number;
+  scoredGoals: number;
+  goalAverage: number;
+}
 export default class TeamRow {
   public id: number;
   public name?: string;
@@ -11,6 +19,24 @@ export default class TeamRow {
     this.reset();
   }
 
+  public toData(): TeamRowProperties {
+    // return Object.fromEntries(Object.entries(this) as any);
+    return {
+      id: this.id,
+      name: this.name,
+      points: this.points,
+      concededGoals: this.concededGoals,
+      scoredGoals: this.scoredGoals,
+      goalAverage: this.goalAverage
+    };
+  }
+
+  public fromData(data: TeamRowProperties): TeamRow {
+    Object.keys(data).forEach((key) => this.set(key, data[key]));
+    return this;
+  }
+
+
   public reset () {
     this.name = null;
     ["points", "concededGoals", "scoredGoals", "goalAverage"]
@@ -20,25 +46,25 @@ export default class TeamRow {
   public set(key: string, value: string): void {
     switch (key) {
       case "name": {
-                     this.name = String(value);
-                     break;
-                   }
+        this.name = String(value);
+        break;
+      }
       case "points": {
-                       this.points = Number(value);
-                       break;
-                     }
+        this.points = Number(value);
+        break;
+      }
       case "concededGoals": {
-                       this.concededGoals = Number(value);
-                       break;
-                     }
+        this.concededGoals = Number(value);
+        break;
+      }
       case "scoredGoals": {
-                       this.scoredGoals = Number(value);
-                       break;
-                     }
+        this.scoredGoals = Number(value);
+        break;
+      }
       case "goalAverage": {
-                       this.goalAverage = Number(value);
-                       break;
-                     }
+        this.goalAverage = Number(value);
+        break;
+      }
     }
   }
 }
