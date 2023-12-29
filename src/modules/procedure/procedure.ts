bc22d79fbdd9fdac30fb8beef841ab6817610c76
Mad Martigane <martigane.mad@gmail.com>
2023-12-29 09:14:20 +0100

    (*): update coding style

--- src/modules/procedure/procedure.ts
+++ src/modules/procedure/procedure.ts
@@ -1,57 +1,59 @@
-import { PROCEDURE_TYPE_ERROR } from "./procedure.constants";
-import { ProcedureData, ProcedureDataType, ProcedureError } from "./procedure.types";
-
+import { PROCEDURE_TYPE_ERROR } from './procedure.constants';
+import { ProcedureData, ProcedureDataType, ProcedureError } from './procedure.types';
 
 export class Procedure {
-    private type: ProcedureDataType | null;
-    private data: unknown | null;
-    private error: ProcedureError | null;
-    private debug: Array<string> | null;
+  private type: ProcedureDataType | null;
+  private data: unknown | null;
+  private error: ProcedureError | null;
+  private debug: Array<string> | null;
 
-    constructor(data: ProcedureData) {
-        if (data) {
-            this.setData(data);
-        }
+  constructor(data: ProcedureData) {
+    if (data) {
+      this.setData(data);
+    }
+  }
+
+  public setData(data: ProcedureData) {
+    this.type = data.procedure;
+    this.data = data.data;
+    this.error = data.error;
+    this.debug = data.debug;
+  }
+
+  public getType(): ProcedureDataType | null {
+    return this.type;
+  }
+
+  public getData(): unknown | null {
+    return this.data;
+  }
+
+  public getLogs(): Array<string> | null {
+    return this.debug;
+  }
+
+  public getError(): ProcedureError | null {
+    return this.error;
+  }
+
+  public isError(): boolean {
+    if (!this.type) {
+      return false;
     }
 
-    public setData(data: ProcedureData) {
-        this.type = data.procedure;
-        this.data = data.data;
-        this.error = data.error;
-        this.debug = data.debug;
+    return PROCEDURE_TYPE_ERROR.includes(this.type);
+  }
+
+  public isOk(): boolean {
+    return !this.isError();
+  }
+
+  public toString(): string {
+    if (!this.type) {
+      return '';
     }
 
-    public getType(): ProcedureDataType | null {
-        return this.type;
-    }
-
-    public getData(): unknown | null {
-        return this.data;
-    }
-
-    public getLogs(): Array<string> | null {
-        return this.debug;
-    }
-
-    public getError(): ProcedureError | null {
-        return this.error;
-    }
-
-    public isError(): boolean {
-        if (!this.type) {
-            return false;
-        }
-
-        return PROCEDURE_TYPE_ERROR.includes(this.type);
-    }
-
-    public isOk(): boolean {
-        return !this.isError();
-    }
-
-    public toString(): string {
-        if (!this.type) { return ""; }
-
-        return this.isOk() ? JSON.stringify(this.data) : (this.error && this.error.message || ""); 
-    }
+    return this.isOk() ? JSON.stringify(this.data) : (this.error && this.error.message) || '';
+  }
 }
+
