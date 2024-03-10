a1941ce27982385cb4d18129cbbd81b4f300fce3
Mad Martigane <martigane.mad@gmail.com>
2024-03-10 15:34:52 +0100

    fix(page-match): display current match selection

--- src/components/match-tile/match-tile.tsx
+++ src/components/match-tile/match-tile.tsx
@@ -1,49 +1,46 @@
-import {
-  Component,
-  Host,
-  Prop,
-  h,
-  State
-} from '@stencil/core';
-import { TeamRow } from "../../modules/team-row/team-row";
+import { Component, Host, Prop, h, State, Watch } from '@stencil/core';
+import { TeamRow } from '../../modules/team-row/team-row';
 
 @Component({
   tag: 'mad-match-tile',
   styleUrl: './match-tile.css',
-  shadow: false
+  shadow: false,
 })
 export class MadMatchTile {
-
   @State() host: TeamRow | null;
   @State() visitor: TeamRow | null;
 
   @Prop() hostPending: Promise<TeamRow | null>;
   @Prop() visitorPending: Promise<TeamRow | null>;
 
+  @Watch('hostPending')
+  private hostPendingChange() {
+    this.hostPending.then((team: TeamRow | null) => {
+      this.host = team;
+    });
+  }
+
+  @Watch('visitorPending')
+  private visitorPendingChange() {
+    this.visitorPending.then((team: TeamRow | null) => {
+      this.visitor = team;
+    });
+  }
+
   constructor() {
-    this.hostPending.then((team: TeamRow | null) => { this.host = team; })
-    this.visitorPending.then((team: TeamRow | null) => { this.visitor = team; })
+    this.hostPendingChange();
+    this.visitorPendingChange();
   }
 
   render() {
     return (
       <Host>
         <ion-row class="ion-align-items-center">
-          <ion-col size="5">
-            {this.host ?
-              <mad-team-tile reverse={true} team={this.host?.team}></mad-team-tile> :
-              <span>loading…</span>
-            }
-          </ion-col>
+          <ion-col size="5">{this.host ? <mad-team-tile reverse={true} team={this.host?.team}></mad-team-tile> : <span>chargement…</span>}</ion-col>
           <ion-col size="2">
             <p> VS </p>
           </ion-col>
-          <ion-col size="5">
-            {this.visitor ?
-              <mad-team-tile team={this.visitor?.team}></mad-team-tile> :
-              <span>loading…</span>
-            }
-          </ion-col>
+          <ion-col size="5">{this.visitor ? <mad-team-tile team={this.visitor?.team}></mad-team-tile> : <span>chargement…</span>}</ion-col>
         </ion-row>
       </Host>
     );
