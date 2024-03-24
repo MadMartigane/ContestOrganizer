623cc5da57ad867e16317226a4e8f994a2546d89
Mad Martigane <martigane.mad@gmail.com>
2024-03-24 13:20:16 +0100

    wip

--- src/global/app.ts
+++ src/global/app.ts
@@ -1,6 +1,13 @@
 import '@ionic/core';
 import { setupConfig } from '@ionic/core';
 
+/* ########### SPECTRUM IMPORT ############### */
+import '@spectrum-web-components/button/sp-button.js';
+import '@spectrum-web-components/button/sp-clear-button.js';
+import '@spectrum-web-components/button/sp-close-button.js';
+
+/* ########### END OF SPECTRUM IMPORT ############### */
+
 /**
  * The code to be executed should be placed within a default function that is
  * exported by the global script. Ensure all of the code in the global script
@@ -9,6 +16,6 @@ import { setupConfig } from '@ionic/core';
 export default async () => {
   setupConfig({
     rippleEffect: true,
-    mode: 'ios'
+    mode: 'ios',
   });
 };
