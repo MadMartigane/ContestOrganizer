d4e3fc643be8b233c6911ac7e7723c20c58b5b65
Mad Martigane <martigane.mad@gmail.com>
2026-03-21 22:05:40 +0100

    chore(api-sports): add constants and API URLs

--- src/modules/api-sports/api-sports.constants.ts
+++ src/modules/api-sports/api-sports.constants.ts
@@ -1,18 +1,16 @@
-import { API_SPORTS_KEY as CONFIG_API_KEY } from "../../config";
+export const LOCAL_STORAGE_TEAM_KEY = "api-sports-teams";
 
-// Re-export with the expected name for backward compatibility
-export const API_SPORTS_KEY = CONFIG_API_KEY;
-
-export const LOCAL_STORAGE_TEAM_KEY = "API_SPORTS_CACHE_TEAMS_V2";
-
-export const LOCAL_STORAGE_TEAM_KEY_LEGACY = "API_SPORTS_CACHE_TEAMS";
-
-export const LEGACY_CACHE_KEYS = ["API_SPORTS_CACHE_TEAMS"];
+export const LEGACY_CACHE_KEYS = [
+  "api-sports-cache",
+  "api-sports-search-cache",
+];
 
 export const URLS = {
-  NFL: "https://v1.american-football.api-sports.io/",
-  RUGBY: "https://v1.rugby.api-sports.io/",
   BASKET: "https://v1.basketball.api-sports.io/",
+  NBA: "https://v1.nba.api-sports.io/",
+  NFL: "https://v1.americanfootball.api-sports.io/",
+  RUGBY: "https://v1.rugby.api-sports.io/",
   FOOT: "https://v3.football.api-sports.io/",
-  NBA: "https://v2.nba.api-sports.io/",
 };
+
+export const API_SPORTS_KEY = import.meta.env.VITE_API_SPORTS_KEY ?? "";
